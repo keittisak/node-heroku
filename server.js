061752032e535590af275d71ca3542831f8b88aa
keittisak <keittisak@movefast.me>
2019-04-09 16:39:02 +0700

    update

--- server.js
+++ server.js
@@ -56,8 +56,7 @@ const postToDialogflow = (req) => {
     req.headers.host = "bots.dialogflow.com";
     return request({
       method: "POST",
-    //   uri: "https://bots.dialogflow.com/line/00a963e0-b8e1-435f-affe-92a6cb6be622/webhook",
-      uri: "https://app.rocketbots.io/line/webhook/1581296513",
+      uri: "https://bots.dialogflow.com/line/00a963e0-b8e1-435f-affe-92a6cb6be622/webhook",
       headers: req.headers,
       body: JSON.stringify(req.body)
     });
