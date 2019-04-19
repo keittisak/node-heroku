96b1537965dac3100a9be33d5b8aeee4d8f5fd4c
keittisak <keittisak@movefast.me>
2019-04-19 14:20:59 +0700

    update

--- controllers/line.js
+++ controllers/line.js
@@ -8,8 +8,7 @@ exports.webhook = async (req, res)=>{
                 var response = await api.reply(req.body);
             } else {
                 var response = await api.postToDialogflow(req);
-                await setTimeout(()=>{}, 3000);
-                await api.push(req.body.events[0].source.userId, JSON.stringify(response));
+                // await api.push(req.body.events[0].source.userId, 'xxx');
                 // var response = await api.postToRocketbot(req);
                 
             }
